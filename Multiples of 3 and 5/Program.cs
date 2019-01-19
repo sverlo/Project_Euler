dc5c150e3cd510f04ff1bc575ff15715384e0264
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-19 15:33:39 +0100

    Adder of Multiples (task 1)

--- Multiples of 3 and 5/Program.cs
+++ Multiples of 3 and 5/Program.cs
@@ -10,6 +10,23 @@ namespace Multiples_of_3_and_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(AdderOfMultiples.GetSum(3, 5, 1000));
+        }
+    }
+
+    class AdderOfMultiples
+    {
+        public static int GetSum(int a, int b, int c)
+        {
+            int sum = 0;
+            for (int i = 0; i < c; i++)
+            {
+                if ((i % a == 0) || (i % b == 0))
+                {
+                    sum += i;
+                }
+            }
+            return sum;
         }
     }
 }
