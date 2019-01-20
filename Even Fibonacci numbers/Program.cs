59127f85f1bb45691135380bce6a6c8cc43033c7
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-20 02:34:33 +0100

    Even Fibonacci numbers (task 2)

---
+++ Even Fibonacci numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Fibonacci_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibonacciNumber.GetSumOfEvenNumbers(4000000));
+        }
+    }
+    class FibonacciNumber
+    {
+        public static int GetSumOfEvenNumbers(int limit)
+        {
+            int sum = 0;
+            int first = 0;
+            int second = 1;
+            int fibonacciNumber = 0;
+
+            while (second <= limit)
+            {
+                fibonacciNumber = first + second;
+                first = second;
+                second = fibonacciNumber;
+
+                if (fibonacciNumber % 2 == 0)
+                {
+                    sum += fibonacciNumber;
+                }
+
+                //Console.WriteLine(fibonacciNumber);
+            }
+            return sum;
+        }
+    }
+}
