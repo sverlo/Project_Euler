17173b23f7b711875357dc3b31d19537726d7209
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-21 18:04:56 +0100

    Smallest multiple (task 5)

---
+++ Smallest multiple/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Largest_prime_factor;
+
+namespace Smallest_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallestMultipleCalculator.GetSmallestMultiple(1, 20));
+        }
+    }
+
+    class SmallestMultipleCalculator
+    {
+        public static int GetSmallestMultiple(int from, int to)
+        {
+            List<int> uniquePrimes = new List<int>();
+            int res = 1;
+
+            for (int i = from; i <= to; i++)
+            {
+                foreach (int prime in PrimeFactorCalculator.GetPrimes(i))
+                {
+                    if (!uniquePrimes.Contains(prime))
+                    {
+                        uniquePrimes.Add(prime);
+                    }
+                }
+            }
+            foreach (int prime in uniquePrimes)
+            {
+                res *= (int)Math.Pow(prime, (int)Math.Floor(Math.Log(to, prime)));
+            }
+            return res;
+        }
+    }
+}
