b833dbcd01c9bcedd3f6ad133c8691fd81d8972f
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-21 18:08:27 +0100

    Remove unused namespaces

--- Smallest multiple/Program.cs
+++ Smallest multiple/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Largest_prime_factor;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Largest_prime_factor;
 
 namespace Smallest_multiple
 {
