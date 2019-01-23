59a2742ed9eb07ac74fdd3a8d0ca4bd49d780c2e
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-23 15:00:42 +0100

    10001st prime (task 7)

---
+++ 10001st prime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Largest_prime_factor;
+
+
+namespace _10001st_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrimesCalculator.GetPrime(10001));
+        }
+    }
+
+    class PrimesCalculator
+    {
+        public static int GetPrime(int primesNumber)
+        {
+            int number = 2;
+            List<int> allPrimes = new List<int>();
+
+            while (allPrimes.Count() != primesNumber)
+            {
+                if (PrimeFactorCalculator.IsPrime(number))
+                {
+                    allPrimes.Add(number);
+                }
+                number++;
+            }
+
+            return allPrimes.ElementAt(primesNumber - 1);
+        }
+    }
+}
