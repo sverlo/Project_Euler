d67e92f699776a76fb403c96a4bca496816add0c
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-31 15:22:43 +0100

    Summation of primes (task 10)

---
+++ Summation of primes/Program.cs
@@ -0,0 +1,28 @@
+﻿using Largest_prime_factor;
+using System;
+
+namespace Summation_of_primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrimesSummator.GetSumOfPrimes(2000000));
+        }
+    }
+    class PrimesSummator
+    {
+        public static long GetSumOfPrimes(int to)
+        {
+            long sum = 0;
+            for(int i = 0; i < to; i++)
+            {
+                if(PrimeFactorCalculator.IsPrime(i))
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
