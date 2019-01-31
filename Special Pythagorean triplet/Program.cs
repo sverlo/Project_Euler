9e7434b416f13815e3b1a03900e616d461c96c49
sverlo <32516435+sverlo@users.noreply.github.com>
2019-01-31 15:07:46 +0100

    Special Pythagorean triplet (task 9)

---
+++ Special Pythagorean triplet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Special_Pythagorean_triplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PythagoreanNumberCalculator.GetProductOfTheSidesOfTriangle(1000));
+        }
+    }
+
+    class PythagoreanNumberCalculator
+    {
+        private static bool IsPythagoreanTriangle(int a, int b, int c)
+        {
+            if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static int GetProductOfTheSidesOfTriangle(int sumOfTheSidesOfTriangle)
+        {
+            int c = 0;
+            for (int a = 1; a < sumOfTheSidesOfTriangle; a++)
+            {
+                for (int b = a; b < sumOfTheSidesOfTriangle - a; b++)
+                {
+                    c = sumOfTheSidesOfTriangle - a - b;
+                    if(IsPythagoreanTriangle(a,b,c))
+                    {
+                        return a * b * c;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Special Pythagorean triplet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Special Pythagorean triplet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Special Pythagorean triplet")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58d6747e-3376-4ccf-8d81-0a06dea870d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
